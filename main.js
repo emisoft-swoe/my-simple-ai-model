d4071b5d5ebf5893919fc1815648af8de311ebff
emisoft-swoe <wenebipadeeminence@gmail.com>
2026-09-24 12:31:24 +0100

    Update main.js

--- main.js
+++ main.js
@@ -133,11 +133,16 @@ function getDatas(type) {
 http.createServer((req, res)=>{
     var message = decodeURI(`${req.url}`.substring(1))
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.writeHead(200, {'content-type':'text/html'})
-    if (message.startsWith('message/'))
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    if (message.startsWith('message/')) {
         res.end(JSON.stringify(generateResponse(message.substring(8))))
-    else fs.readFile('./index.html', {encoding:'utf-8'}, (err, data)=>{
-        if (!err) res.end(data)
-        else res.end('Page not found!')
-    });
+        res.writeHead(200, {'Content-Type':'application/json'})
+    } else {
+        res.writeHead(200, {'Content-Type':'text/html'})
+        fs.readFile('./index.html', {encoding:'utf-8'}, (err, data)=>{
+            if (!err) res.end(data)
+            else res.end('Page not found!')
+        });
+    }
 }).listen(process.env.PORT)
